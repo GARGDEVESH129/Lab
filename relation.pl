6afe37870d9842027723196b8d21c5aadaafd5bd
Devesh Garg <92987678+GARGDEVESH129@users.noreply.github.com>
2025-05-19 00:08:55 +0530

    Create relation.pl

---
+++ relation.pl
@@ -0,0 +1,39 @@
+%Facts
+male(ajay).
+male(bobby).
+male(chatur).
+male(david).
+male(akash).
+female(emma).
+female(lisa).
+female(mary).
+female(jerry).
+female(miya).
+parent(ajay,bobby).
+parent(bobby,lisa).
+parent(mary,jerry).
+parent(jerry,lisa).
+parent(akash,miya).
+parent(ajay,david).
+
+
+
+%Rules
+father(X,Y):- parent(X,Y),
+        male(X).
+
+mother(X,Y):- parent(X,Y),
+         female(X).
+
+brother(X,Y):- parent(Z,X),
+          parent(Z,Y),
+          male(X),
+          Y\==Z.
+
+sister(X,Y):- parent(Z,X),
+          parent(Z,Y),
+          female(X),
+          Y\==Z.
+
+grandfather(X,Y):- father(X,Z),
+              parent(Z,Y).
